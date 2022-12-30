55987cd7eae711a1c2de8266a37c2f52078f21da
sunetro123 <20804544+sunetro123@users.noreply.github.com>
2022-12-30 23:08:11 +0000

    fixing pylint

--- 043MultiplyStrings/multiply_strings.py
+++ 043MultiplyStrings/multiply_strings.py
@@ -1,5 +1,7 @@
-# TRICK -> In result DETERMINE the Postion as outer_loop-iter_number + inner_loop_iter_number 
-
+# TRICK -> In result DETERMINE the Postion as outer_loop-iter_number + inner_loop_iter_number
+# pylint: disable=missing-module-docstring
+# pylint: disable=missing-function-docstring
+# pylint: disable=invalid-name
 """
 Given two non-negative integers num1
 and num2 represented as strings, return the product of num1 and num2.
@@ -16,13 +18,7 @@ You must not use any built-in BigInteger library or convert the inputs to intege
 import logging
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
-"""
-    num1 and num 2 both array of string 
-    in any multiplication result can have digits len(num1) + len(num2)
-    Create an array of that size all zero 
-    Take the bigger number and reverse it 
-    take the samller number and reverese it 
-"""
+
 
 if __name__ == "__main__":
     print("Trying out github codedev first time")
@@ -38,21 +34,19 @@ if __name__ == "__main__":
     print("".join(reversed(num2)))
     rev_num1 = "".join(reversed(num1))
     rev_num2 = "".join(reversed(num2))
-    for pos1 in range(0, len(rev_num1)):
+    for pos1,val1  in enumerate(rev_num1):
         operand1 = int(rev_num1[pos1])
-        for pos2 in range(0, len(rev_num2)):
-            res_array_marker = pos1 + pos2 
+        for pos2, val2 in enumerate(rev_num2):
+            res_array_marker = pos1 + pos2
             operand2 = int(rev_num2[pos2])
-            print(f"operand 1 --> {operand1} , operand 2 -> {operand2} \n ======================================****==============\n")
+            print(f"operand 1 --> {operand1} , operand 2 -> {operand2} \n")
+            print(" ======================================****==============\n")
             run_res = (operand1 * operand2 ) + result_array[res_array_marker]
             print(f"\nrunning result -> {run_res}")
             pos_res = int(run_res % 10)
             pos_cover = int(run_res / 10)
             result_array[res_array_marker] = pos_res
             result_array[res_array_marker + 1] += pos_cover
-            print(f"result array now -> {result_array} \n ============= one iteration complete ===================== \n")
-    
+            print(f"result array now -> {result_array} \n ")
+            print("============= one iteration complete ===================== \n")
     print(f" final result -> {[str(x) for x in reversed(result_array)]}")
-        
-
-    
