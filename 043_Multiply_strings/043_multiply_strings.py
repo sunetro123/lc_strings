4df574202b3593c47df05b91b6a8aa319c1fa294
sunetro123 <20804544+sunetro123@users.noreply.github.com>
2022-12-30 21:19:22 +0000

    comminting_043

---
+++ 043_Multiply_strings/043_multiply_strings.py
@@ -0,0 +1,19 @@
+"""
+Given two non-negative integers num1 and num2 represented as strings, return the product of num1 and num2.
+
+Note:
+
+The length of both num1 and num2 is < 110. 
+Both num1 and num2 contains only digits 0-9. 
+Both num1 and num2 does not contain any leading zero. 
+You must not use any built-in BigInteger library or convert the inputs to integer directly.
+
+"""
+
+import logging
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+if __name__ == "__main__":
+    print("Trying out github codedev first time")
+    logger.debug("Trying out github codedev first time")
